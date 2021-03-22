6b617fc5e69fc27dd74d70d724c53fb81660e514
Cameron Ratliff <cratliff@colgate.edu>
2021-03-22 19:13:48 -0400

    part way through

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const app = Vue.createApp({
-  template: '<h1>Hello World</h1>',
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@gamil.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+    }
+  },
 })
 
 app.mount('#app')
